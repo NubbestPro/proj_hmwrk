1b6ce9e4731ba9079a426e7b7d8757192f802f24
LAPTOP-LBM6I7BH\gavin <gavinchen1003@gmail.com>
2021-02-15 17:56:42 -0500

    initial setup

--- proj_hmwrk/settings.py
+++ proj_hmwrk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2d+7122w^j3*6-p$vdl*m752#7)yia@pxul4)o%r8_nb+330)8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['NubbestPro1.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app03',
+    'app3',
 ]
 
 MIDDLEWARE = [
